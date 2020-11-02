daeea775343e668493199fa012c23437ede8a526
Kenta Iwasaki <kenta@lithdew.net>
2020-11-03 00:14:21 +0900

    iocp, poll: initial work on poll(), add GetQueuedCompletionStatusEx()

--- poll.zig
+++ poll.zig
@@ -1,11 +1,17 @@
 const std = @import("std");
 const afd = @import("afd.zig");
+const iocp = @import("iocp.zig");
 
 const math = std.math;
+const testing = std.testing;
 
 const os = std.os;
 const windows = os.windows;
 const ws2_32 = windows.ws2_32;
+
+pub const Handle = struct {
+    inner: windows.HANDLE,
+};
 
 pub const Poller = struct {
     const Self = @This();
@@ -32,7 +38,21 @@ pub const Poller = struct {
         self.driver.deinit();
         windows.CloseHandle(self.port);
     }
+
+    pub fn poll(self: *const Self) !void {
+        var events: [1024]iocp.OVERLAPPED_ENTRY = undefined;
+
+        const num_events = try iocp.GetQueuedCompletionStatusEx(self.port, &events, null, true);
+
+        for (events[0..num_events]) |event| {
+            std.debug.print("IOCP Notification {}\n", .{event});
+        }
+    }
 };
+
+test "" {
+    testing.refAllDecls(@This());
+}
 
 test "Poller.init() / Poller.deinit()" {
     const poller = try Poller.init("Test");
